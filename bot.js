4e682d62287dee521b37db40be002b4f8bd8d472
ItxSimply <45908892+ItxSimply@users.noreply.github.com>
2018-12-16 11:41:07 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,14 @@
 const Discord = require("discord.js") 
 const settings = require("./your_settings.json")
 const bot = new Discord.Client()
+
+
 bot.on('ready', async => {
-console.log("Rainbow bot is ready!" + "\n" + bot.user.tag + "\n" + "Server Count: "  + bot.guilds.size + "\n" + "Cached users: " + bot.users.size + "\n" + "Enjoy!")
+    console.log("Rainbow bot is ready!" + "\n" + bot.user.tag + "\n" + "Server Count: "  + bot.guilds.size + "\n" + "Cached users: " + bot.users.size + "\n" + "Enjoy!")
+    bot.user.setStatus('dnd')
+    bot.user.setGame('with SimplyChristmas#2656 [*]');
 });
+
 bot.on('message', message => {
     let messageArray = message.content.split(" ");
     let command = messageArray[0];
